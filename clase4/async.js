d840bbdc7679c5b1cc1afc05eaa2d151ff2adfa1
carlos <crdgom@gmail.com>
2022-12-06 18:56:46 -0600

    clase4

---
+++ clase4/async.js
@@ -0,0 +1,12 @@
+async function sencillita() {
+  return 89;
+}
+
+let p = sencillita();
+
+let p2 = await sencillita();
+
+/* p.then((valor) => {
+  console.log("El valor que retorna la promesa es " + valor);
+}); */
+
